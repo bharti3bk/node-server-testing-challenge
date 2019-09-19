9063eda844bd9a4116a2a946c9fe504cd7699f4e
bharti3bk <bharti3bk@gmail.com>
2019-09-19 13:09:05 -0700

    Added helper functions and endpoints in the project

--- Users/userModel.js
+++ Users/userModel.js
@@ -1,4 +1,21 @@
 const express = require("express");
+const db = require('../Data/db_config.js');
 const server = express();
-server.use(express.json());
+server.use(express.json()); 
+module.exports = {
+   insert , 
+   remove
+} 
+
+function insert(user) {
+    return db('users')
+      .insert(user)
+      .then(ids => ({ id: ids[0] }));
+  } 
+
+  function remove(id) {
+    return db('users')
+      .where('id', Number(id))
+      .del();
+  }
 
